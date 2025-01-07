b0ed6caba0f0dfbaf1e881cf71154d0529991dcd
Kevin Gustavo Collado <kcolladojimenez@gmail.com>
2025-01-06 22:00:33 -0600

    Update: AppDelegate.swift
    Se configura la pantalla inicial

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -13,7 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //Se crea una instancia de UIWindow, que ocupe toda la pantalla del dispositivo, se instancía MainViewController() y se pasa como controlador inicial de vista, se vuelve llave y visible, se pasa la instancia a window.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = MainViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 
