a1ab22b81898e3524aa2548d87b16c2f0baf2814
Kevin Gustavo Collado <kcolladojimenez@gmail.com>
2025-02-11 23:20:53 -0600

    Update: AppDelegate
    Add key GooglePlaces

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        //Key GooglePlaces
+        GMSPlacesClient.provideAPIKey("AIzaSyAmtz5WIJNYD6PYictdy0KLGVoYVlJbGRE")
         
         //Se crea una instancia de UIWindow, que ocupe toda la pantalla del dispositivo, se instancía MainViewController() y se pasa como controlador inicial de vista, se vuelve llave y visible, se pasa la instancia a window.
         let window = UIWindow(frame: UIScreen.main.bounds)
--- Controllers/MainViewController/MainViewController.swift
+++ Controllers/MainViewController/MainViewController.swift
@@ -26,5 +26,4 @@ class MainViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
